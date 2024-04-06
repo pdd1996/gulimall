9f0e90964bc4af476f4772a32baf8723245482d5
paddy <pdd337816464@163.com>
2024-04-06 10:11:49 +0800

    add test

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("123");
+//        brandEntity.setName("华为");
+//        brandService.save(brandEntity);
+        brandService.updateById(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
