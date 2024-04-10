623ada0dba28dddd6b9fe108b65981971205dedc
Paddy_Pan <pdd337816464@outlook.com>
2024-04-10 21:37:35 +0800

    add member application.yml and pom

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -1,9 +1,12 @@
 package com.atguigu.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.atguigu.gulimall.member.dao")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
